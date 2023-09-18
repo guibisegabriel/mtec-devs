e5c6bbe397f6e234b2a9f9c7cd663067994d3a91
Guilherme Bispo e Gabriel <guilherme19.b07@gmail.com>
2023-09-18 09:33:52 -0300

    criação da pasta data

---
+++ MtecDevs/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MtecDevs.Models;
+
+namespace MtecDevs.Data;
+
+public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions opcoes) : base(opcoes)
+    {
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; } 
+    public DbSet<TipoDev> TipoDevs { get; set; }
+}
+
+
+
+    
+    
+        
+    
--- MtecDevs/Models/Usuario.cs
+++ MtecDevs/Models/Usuario.cs
@@ -1,11 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace MtecDevs.Models;
 
 
 public class Usuario
 {
+
+    [Key]
+    public string UserId {get; set;}
+    [ForeignKey("UserId")]
+    public IdentityUser AccountUser {get; set;}
+
+
     [Required(ErrorMessage ="Informe o Nome")] 
     [StringLength(30,ErrorMessage = "O nome deve possuir no máximo 30 caracteres")]
     public string Nome {get; set;}
