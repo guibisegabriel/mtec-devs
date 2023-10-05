ce962f697bb56b109bc5a49a632e643f91c86b13
GUilherme Bispo e Gabriel <guilherme19.b07@gmail.com>
2023-10-05 12:19:06 -0300

    termino compulação de dados

--- MtecDevs/Data/AppDbSeed.cs
+++ MtecDevs/Data/AppDbSeed.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MtecDevs.Models;
-
 namespace MtecDevs.Data;
+
+public class NewBaseType
+{
+}
 
 public class AppDbSeed
 {
@@ -63,8 +66,8 @@ public class AppDbSeed
         List<IdentityUser> users = new() {
             new IdentityUser() {
                 Id = Guid.NewGuid().ToString(),
-                UserName = "Adm",
-                NormalizedUserName = "ADM",
+                UserName = "Guilherme Bispo",
+                NormalizedUserName = "GUILHERME BISPO",
                 Email = "guilherme19.b07@gmail.com",
                 NormalizedEmail = "GUILHERME19.B07@GMAIL.COM",
                 EmailConfirmed = true,
@@ -72,11 +75,40 @@ public class AppDbSeed
             }
         };
         builder.Entity<IdentityUser>().HasData(users);
+        #endregion
+        
         //criptografar a senha do IdentityUser
+        #region 
+        foreach (var user in users)
+        {
+            PasswordHasher<IdentityUser> password = new();
+            user.PasswordHash = password.HashPassword(user, "@Etec123");
+        }
+        builder.Entity<IdentityUser>().HasData(users);
+        #endregion
 
-        //Criar a senha
-
-        //definir o perfil do Usuario Criado
+        //criar o usuario
+        #region Criação de usuário
+        List<Usuario> usuarios = new();{
+            Usuario usuario = new Usuario() {
+                UserId = users[0].Id,
+                Nome = "Guilherme Bispo",
+                DataNascimento = DateTime.Parse("19/09/2006"),
+                TipoDevId = 3
+            };
+        };
+        builder.Entity<Usuario>().HasData(usuarios);
+        #endregion
+        
+        //Definir o perfil do Usuario Criado
+        #region Definir perfil do Usuario
+        List<IdentityUserRole<string>> userRoles = new(){
+            new IdentityUserRole<string>(){
+                UserId = users[0].Id,
+                RoleId = perfis[0].Id
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
         #endregion
     }
 }
