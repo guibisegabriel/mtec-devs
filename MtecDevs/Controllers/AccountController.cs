afc00df11ec869b3b6b8e96d37d306fbec195cce
Gabriel e Guilherme Bispo <guilherme19.b07@gmail.com>
2023-11-06 09:40:18 -0300

    codificando o login

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,25 +1,91 @@
+using System.Net.Mail;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using MtecDevs.ViewModels;
 
 namespace MtecDevs.Controllers;
 
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
-   
-    public AccountController(ILogger<AccountController> logger)
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
     {
         _logger = logger;
-    }
- [HttpGet]
-    public IActionResult Login()
-    {
-        return View();
+        _signInManager = signInManager;
+        _userManager = userManager;
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpGet]
+    public IActionResult Login(string ReturnUrl)
     {
-        return View("Error!");
+        LoginVM login = new(){
+            UrlRetorno = ReturnUrl ?? Url.Content("~/")
+        };
+        return View(login);
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            // Verifica o Login
+            string userName = login.Email;
+            // Verificando se o login é por email
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+            // Login é só por UserName
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+            
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                return RedirectToAction("Lockout");
+            }
+            ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!");
+        }
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
 }
